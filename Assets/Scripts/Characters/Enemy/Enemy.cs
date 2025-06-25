5368e25427d400d1c8e3d1001ddcede761718ec7
coldborne <coldborneVV@gmail.com>
2025-06-25 20:04:01 +0300

    Enemy: Добавил возможность преследовать врагом игрока, если тот приблизится к нему Если игрок отходит, то враг возвращается к патрулированию

--- Assets/Scripts/Characters/Enemy/Enemy.cs
+++ Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -1,21 +1,81 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Enemies
 {
     public class Enemy : Unit
     {
+        private enum State
+        {
+            Patrolling,
+            Chasing
+        }
+
+        [Header("Патрулирование")] 
         [SerializeField] private Collection _targetPoints;
+
         [SerializeField, Min(1f)] private float _arrivalThreshold;
 
+        [Header("Преследование")] 
+        [SerializeField] private float _loseTargetDelay;
+
         private Transform _targetPoint;
+
+        private Transform _target;
+        private Coroutine _losePlayerCoroutine;
+
+        private State _state;
 
         protected override void Awake()
         {
             base.Awake();
             _targetPoint = _targetPoints.Current;
+            _state = State.Patrolling;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.TryGetComponent<Player>(out _))
+            {
+                _target = collider.transform;
+                _state = State.Chasing;
+
+                if (_losePlayerCoroutine != null)
+                {
+                    StopCoroutine(_losePlayerCoroutine);
+                    _losePlayerCoroutine = null;
+                }
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collider)
+        {
+            if (collider.TryGetComponent<Player>(out _))
+            {
+                if (_losePlayerCoroutine != null)
+                {
+                    StopCoroutine(_losePlayerCoroutine);
+                }
+
+                _losePlayerCoroutine = StartCoroutine(LosePlayerCountDown());
+            }
         }
 
         private void Update()
+        {
+            switch (_state)
+            {
+                case State.Patrolling:
+                    Patrol();
+                    break;
+
+                case State.Chasing:
+                    ChaseTarget();
+                    break;
+            }
+        }
+
+        private void Patrol()
         {
             float sqrDistance = (transform.position - _targetPoint.position).sqrMagnitude;
 
@@ -24,12 +84,12 @@ namespace Enemies
                 _targetPoint = _targetPoints.MoveNext();
             }
 
-            MoveStep();
+            MoveTowards(_targetPoint);
         }
 
-        private void MoveStep()
+        private void MoveTowards(Transform target)
         {
-            if (IsRightOf(_targetPoint))
+            if (IsRightOf(target))
             {
                 Move((float)Directions.Left);
             }
@@ -37,6 +97,20 @@ namespace Enemies
             {
                 Move((float)Directions.Right);
             }
+        }
+
+        private void ChaseTarget()
+        {
+            MoveTowards(_target);
+        }
+
+        private IEnumerator LosePlayerCountDown()
+        {
+            yield return new WaitForSeconds(_loseTargetDelay);
+
+            _target = null;
+            _state = State.Patrolling;
+            _losePlayerCoroutine = null;
         }
     }
 }
