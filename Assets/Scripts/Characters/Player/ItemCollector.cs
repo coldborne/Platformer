9a90a73e3129eb1634fc2e6d049f8e9eed937828
coldborne <coldborneVV@gmail.com>
2025-06-25 11:11:42 +0300

    Реорганизация проекта, распределил скрипты по папкам
    ItemCollector:
    Убрал у поля _objectDestroyer атрибут [SerializeField], переделал через RequireComponent

--- Assets/Scripts/Characters/Player/ItemCollector.cs
+++ Assets/Scripts/Characters/Player/ItemCollector.cs
@@ -1,12 +1,18 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(ObjectDestroyer))]
 public class ItemCollector : MonoBehaviour
 {
-    [SerializeField] private ObjectDestroyer _objectDestroyer;
-    
+    private ObjectDestroyer _objectDestroyer;
+
     public event Action<int> CoinCollected;
-    
+
+    private void Awake()
+    {
+        _objectDestroyer = GetComponent<ObjectDestroyer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Coin coin))
